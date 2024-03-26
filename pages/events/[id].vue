9b5279d86d8af99330c1452240b7c6c27d48f391
YurGoV <yurgov@gmail.com>
2024-03-26 10:44:46 +0200

    pages

---
+++ pages/events/[id].vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { useRoute } from "vue-router";
+// import { useNuxtApp } from "nuxt";
+
+const route = useRoute();
+console.log(route.params.id);
+
+const nuxt = useNuxtApp();
+console.log("useNuxtApp", nuxt);
+</script>
+
+<template>
+  <div>event id page</div>
+  <div>event id: {{ $route.params.id }}</div>
+</template>
