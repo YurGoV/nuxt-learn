f87fc0cf209f85a8f0344a51aed1a61d21e9a9cb
YurGoV <yurgov@gmail.com>
2024-03-27 11:00:59 +0200

    composables

---
+++ composables/useUtils.ts
@@ -0,0 +1,10 @@
+// NOTE: good practice to do business logic in composables
+export const useUtils = () => {
+  const sayHello = () => console.log("Hello");
+  return {
+    sayHello,
+  };
+};
+
+// TODO: research VueUse library (to shorten dev time)
+// https://vueuse.org/
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,9 @@
+<script setup lang="ts">
+// @ts-ignore
+const { sayHello } = useUtils();
+console.log('pre-hello string')
+sayHello()
+</script>
 <template>
   <div class="Main">
     <IconsBicycle />
